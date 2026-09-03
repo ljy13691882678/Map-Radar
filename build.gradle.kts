a4e71f7625a0009a1c0d88acb8aacb26165f4c2e
ljy13691882678 <ljy13691882678@users.noreply.github.com>
2026-09-03 15:46:31 +0000

    chore: drop shadow plugin (not needed, using native Jar task)

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,5 +8,4 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.compose.multiplatform) apply false
-    alias(libs.plugins.shadow) apply false
 }
